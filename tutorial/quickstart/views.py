6e92ffe2f570ccddb9fb193ca8277d6edcc4f6bf
Matías Lang <matias.lang@vixionar.com>
2015-02-10 15:47:56 -0300

    this is the problem

--- tutorial/quickstart/views.py
+++ tutorial/quickstart/views.py
@@ -1,12 +1,23 @@
 from django.contrib.auth.models import User, Group
 from rest_framework.permissions import DjangoModelPermissions
 from rest_framework import viewsets
+from rest_framework.decorators import detail_route, list_route
+from rest_framework.response import Response
 from tutorial.quickstart.serializers import UserSerializer, GroupSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    @detail_route(methods=['GET'], url_path='show/encrypted_password')
+    def display_password(self, request, pk):
+        obj = self.get_object()
+        return Response(obj.password)
+
+    @list_route(methods=['GET'], url_path="show_12345")
+    def test_list(self, request):
+        return Response(12345)
 
 
 class GroupViewSet(viewsets.ModelViewSet):
